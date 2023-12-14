59f24cc0eea35f6297bb84ae08e92fb82b12a2b2
Ajay Kaarthic Jeysree <ajay.kaarthic@gmail.com>
2023-12-14 12:30:18 -0600

    basic web app

---
+++ dockerfile
@@ -0,0 +1,9 @@
+FROM node:latest
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install --verbose
+COPY . .
+EXPOSE 3000
+CMD ["npm", "start"]
+
+
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
