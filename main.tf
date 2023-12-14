13348fc3744c89afec88acdaf8f11c14dbd4eec3
Ajay Kaarthic Jeysree <ajay.kaarthic@gmail.com>
2023-12-14 16:23:22 -0600

    adding terraform code

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0230bd60aa48260c6"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "terraform-example"
+    }
+}
